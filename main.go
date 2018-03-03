32ba4760506be284fbcdee9808d9f2cdbac3a325
Alexander Demichev <ademicev@redhat.com>
2018-03-03 18:25:14 +0100

    run bot from main

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"time"
+  "log"
+	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
 
 type userInfo struct {
   Phone string `json:"Phone"`
@@ -10,11 +18,26 @@ type userInfo struct {
 
 
 func main() {
+	b, err := tb.NewBot(tb.Settings{
+	Token: os.Getenv("TELEGRAM_API_TOKEN"),
+	Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	user := &userInfo{}
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	handleStart(b, user)
+
+	b.Start()
 	// SendSMS("+420725543082", "ty pidor")
 	// GetCryptoRates()
-	SaveUser(userInfo{
-		Phone: "+420725543082",
-		Currency: "BTC",
-		Percentage: "10"})
-	UpdateCurrencyPrice()
+// 	SaveUser(userInfo{
+// 		Phone: "+420725543082",
+// 		Currency: "BTC",
+// 		Percentage: "10"})
+// 	UpdateCurrencyPrice()
 }
